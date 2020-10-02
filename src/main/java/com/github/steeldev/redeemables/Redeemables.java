22a9fbb06b880e6017981e0b6359a41ba30ccbc4
Steel_Dev <reals7eel02@gmail.com>
2020-10-01 21:50:19 -0400

    new colorize function (thanks Shane)

--- src/main/java/com/github/steeldev/redeemables/Redeemables.java
+++ src/main/java/com/github/steeldev/redeemables/Redeemables.java
@@ -10,9 +10,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Redeemables extends JavaPlugin {
     public static Redeemables instance;
+
+    private static final Pattern HEX_PATTERN = Pattern.compile("<#([A-Fa-f0-9]){6}>");
 
     public FileConfiguration config = null;
     public FileConfiguration redeemCodes = null;
@@ -58,6 +62,14 @@ public class Redeemables extends JavaPlugin {
     }
 
     public String colorize(String string) {
+        Matcher matcher = HEX_PATTERN.matcher(string);
+        while (matcher.find()) {
+            final net.md_5.bungee.api.ChatColor hexColor = net.md_5.bungee.api.ChatColor.of(matcher.group().substring(1, matcher.group().length() - 1));
+            final String before = string.substring(0, matcher.start());
+            final String after = string.substring(matcher.end());
+            string = before + hexColor + after;
+            matcher = HEX_PATTERN.matcher(string);
+        }
         return ChatColor.translateAlternateColorCodes('&', string);
     }
 
